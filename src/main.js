2790474d977c5bf0771cb953fae6d3be2a246272
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-01-30 05:21:29 -0500

    additional items for init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import AppDropdown from './components/shared/AppDropdown';
+import AppHero from './components/shared/AppHero';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.component('AppHero', AppHero);
+Vue.component('AppDropdown', AppDropdown);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount('#app');
