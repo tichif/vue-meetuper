98ca2c27bd2bc4aa59c50ad73a454eff6a2fc104
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-02-25 20:14:54 -0500

    register implementation

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,13 +1,51 @@
 const User = require('../models/users');
 
 exports.getUsers = function(req, res) {
-  User.find({})
-        .exec((errors, users) => {
-
+  User.find({}).exec((errors, users) => {
     if (errors) {
-      return res.status(422).send({errors});
+      return res.status(422).send({ errors });
     }
 
     return res.json(users);
   });
-}
+};
+
+exports.register = function(req, res) {
+  const registerData = req.body;
+
+  // check if exists email
+  if (!registerData.email) {
+    return status(422).json({
+      errors: {
+        email: 'is required'
+      }
+    });
+  }
+
+  // check if exists password
+  if (!registerData.password) {
+    return status(422).json({
+      errors: {
+        password: 'is required'
+      }
+    });
+  }
+
+  // check if password match with confirmPassword
+  if (registerData.password !== registerData.passwordConfirmation) {
+    return status(422).json({
+      errors: {
+        password: 'is not the same with confirmation password'
+      }
+    });
+  }
+
+  // create users
+  const user = new User(registerData);
+  return user.save((errors, saveUser) => {
+    if (errors) {
+      return res.status(422);
+    }
+    return res.json(saveUser);
+  });
+};
